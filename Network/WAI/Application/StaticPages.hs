b48c946d733cfa7d920fde7eedccc77c0da95e9f
Greg Weber <greg@gregweber.info>
2013-06-16 09:28:23 -0700

    use Text

--- Network/WAI/Application/StaticPages.hs
+++ Network/WAI/Application/StaticPages.hs
@@ -1,3 +1,4 @@
+{-# Language OverloadedStrings #-}
 module Network.WAI.Application.StaticPages (
     parseRoutePaths
   , renderStaticPages
@@ -11,27 +12,34 @@ import Data.Conduit (runResourceT)
 import Blaze.ByteString.Builder (toLazyByteString)
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Lazy as LBS
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
+import Data.Monoid (mappend)
 
-renderStaticPages :: [Char] -> [String] -> Application -> IO ()
-renderStaticPages directory requests app = do
+renderStaticPages :: Application -> Text -> [Text] -> IO ()
+renderStaticPages app directory requests = do
   flip mapM_ requests $ \path -> do
     let p = noFrontSlash $ noTrailSlash path
-    let req = setRawPathInfo defaultRequest $ BS8.pack p 
-    let outPath = (directory ++ (BS8.unpack $ rawPathInfo req) ++ ".html")
+    let req = setRawPathInfo defaultRequest $ encodeUtf8 p 
+    let outPath = directory `mappend` (decodeUtf8 $ rawPathInfo req) `mappend` ".html"
     print (p, outPath)
     rsp <- runResourceT $ app req
     case rsp of
       ResponseBuilder s h b ->
         let body = toLazyByteString b
         in  if s /= H.status200 then error $ "unexpected status: " ++ show s ++ "\nheaders: " ++ show h ++ "\nbody: " ++ show body else
-              LBS.writeFile outPath body 
+              LBS.writeFile (T.unpack outPath) body 
       _ -> error "expected ResponseBuilder"
 
-noTrailSlash :: String -> String
-noTrailSlash = reverse . noFrontSlash . reverse
+noTrailSlash :: Text -> Text
+noTrailSlash str | T.null str = str
+noTrailSlash str | T.last str == '/' = T.init str
+noTrailSlash str = str
 
-noFrontSlash :: String -> String
-noFrontSlash ('/':str) = str
+noFrontSlash :: Text -> Text
+noFrontSlash str | T.null str = str
+noFrontSlash str | T.head str == '/' = T.tail str
 noFrontSlash str = str
 
 -- | Conveniently specify paths through nested indentation
@@ -40,7 +48,7 @@ noFrontSlash str = str
 --
 -- > import Shakespeare.Text (st)
 -- >
--- > staticPaths = parseRoutePaths $ T.unpack [st|
+-- > staticPaths = parseRoutePaths [st|
 -- >/pages
 -- >        about
 -- >        faq
@@ -48,11 +56,11 @@ noFrontSlash str = str
 -- >|]
 --
 -- > staticPaths == ["/pages/about", "/pages/faq", "/pages"]
-parseRoutePaths :: String -> [String]
+parseRoutePaths :: Text -> [Text]
 parseRoutePaths =
-    parse [(0, "")] . filter (not . null) . lines
+    parse [(0, "")] . filter (not . T.null) . T.lines
   where
-    parse :: [(Int, String)] -> [String] -> [String]
+    parse :: [(Int, Text)] -> [Text] -> [Text]
     parse _ [] = []
     parse prefixes (line:[]) = [snd $ parseLine prefixes line]
     parse prefixes (line:nextLine:otherLines) =
@@ -63,12 +71,12 @@ parseRoutePaths =
             then rest
             else parsed : rest
 
-    parseLine :: [(Int, String)] -> String -> ([(Int, String)], String)
+    parseLine :: [(Int, Text)] -> Text -> ([(Int, Text)], Text)
     parseLine prefixes line = (prefixes', parsed)
       where
         (smaller_prefixes, _) = partition (\(indent, _) -> numSpaces > indent) prefixes
-        numSpaces = length spaces
-        spaces = takeWhile (== ' ') line
+        numSpaces = T.length spaces
+        spaces = T.takeWhile (== ' ') line
 
         prefixes' =
           if null smaller_prefixes || numSpaces > (fst $ last smaller_prefixes)
@@ -78,9 +86,9 @@ parseRoutePaths =
 
         parsed = foldl1 (</>) (map snd smaller_prefixes ++ [path])
           where
-            a </> b = noTrailSlash a ++ "/" ++ noFrontSlash b
+            a </> b = noTrailSlash a `mappend` "/" `mappend` noFrontSlash b
 
         path =
-          case words line of
+          case T.words line of
               [p] -> p
-              _ -> error $ "Invalid line: " ++ line
+              _ -> error $ "Invalid line: " ++ show line
